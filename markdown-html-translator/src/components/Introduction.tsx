cebfb7c4beb8a93f103521a077d9fb01e86d1287
Francisco <francisco.piqueras.smr@gmail.com>
2025-04-02 16:51:20 +0200

    Cosas arregladas

--- markdown-html-translator/src/components/Introduction.tsx
+++ markdown-html-translator/src/components/Introduction.tsx
@@ -2,21 +2,37 @@ import { Link } from "react-router";
 import Header from "./Header";
 import IntroductionContent from "./IntroductionContent";
 import IntroductionSidebar from "./IntroductionSidebar";
+import { useEffect, useState } from "react";
 
 const Introduction = () => {
+    const [windowSize, setWindowSize] = useState({
+        width: window.innerWidth
+    });
+
+    useEffect(() => {
+        const handleResize = () => {
+            setWindowSize({
+                width: window.innerWidth,
+            });
+        };
+
+        window.addEventListener('resize', handleResize);
+        return () => window.removeEventListener('resize', handleResize);
+    }, []);
+
     return (
         <div className="background-introduction">
             <div className="introduction-container">
                 <Header />
                 <div className="main__introduction">
                     <IntroductionContent />
-                    <div></div>
-                    <IntroductionSidebar />
+                    {windowSize.width > 820 && <div></div>}
+                    {windowSize.width > 820 && <IntroductionSidebar />}
                 </div>
                 <footer className="footer__introduction">
                     <Link to="/">
                         <span>Ir a Markdown</span>
-                        <svg viewBox="0 0 16 16" fill="currentColor"><path fill-rule="evenodd" d="M6.22 4.22a.75.75 0 0 1 1.06 0l3.25 3.25a.75.75 0 0 1 0 1.06l-3.25 3.25a.75.75 0 0 1-1.06-1.06L8.94 8 6.22 5.28a.75.75 0 0 1 0-1.06Z" clip-rule="evenodd"></path></svg>
+                        <svg viewBox="0 0 16 16" fill="currentColor"><path fillRule="evenodd" d="M6.22 4.22a.75.75 0 0 1 1.06 0l3.25 3.25a.75.75 0 0 1 0 1.06l-3.25 3.25a.75.75 0 0 1-1.06-1.06L8.94 8 6.22 5.28a.75.75 0 0 1 0-1.06Z" clipRule="evenodd"></path></svg>
                     </Link>
                 </footer>
             </div>
--- markdown-html-translator/src/components/IntroductionContent.tsx
+++ markdown-html-translator/src/components/IntroductionContent.tsx
@@ -29,111 +29,92 @@ const IntroductionContent = () => {
                     Para crear un encabezado, agrega entre uno y seis símbolos <code>#</code> antes del encabezado del texto.
                     El número de <code>#</code> que utilices determinará el nivel jerárquico y el tamaño tipográfico del encabezado.
                 </p>
-                <pre>
-                    <code>
-                        <p># Primer nivel</p>
-                        <p>## Segundo nivel</p>
-                        <p>### Tercer nivel</p>
-                    </code>
-                </pre>
+                <p><code>Primer nivel</code></p>
             </IntroductionSection>
             <IntroductionSection idSection="estilosTexto" hrefValue="#estilosTexto" titleSection={"Estilos de texto"}>
                 <p>
                     Puedes indicar énfasis con texto en negrita, cursiva, tachado, o de subíndice o superíndice en los campos
                     de comentarios y archivos .md.
                 </p>
-                <p>
-                    <table className="tabla__estilos__markdown">
-                        <thead>
-                            <tr>
-                                <th>Estilo</th>
-                                <th>Sintaxis</th>
-                                <th>Ejemplo</th>
-                                <th>Resultados</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr>
-                                <td><strong>Negrita</strong></td>
-                                <td><code>&lt;strong&gt; &lt;/strong&gt;</code></td>
-                                <td><code>&lt;strong&gt;Este es un texto en negrita&lt;/strong&gt;</code></td>
-                                <td><strong>Este es un texto en negrita.</strong></td>
-                            </tr>
-                            <tr>
-                                <td><em>Cursiva</em></td>
-                                <td><code>&lt;em&gt; &lt;/em&gt;</code></td>
-                                <td><code>&lt;em&gt;Este texto está en cursiva&lt;/em&gt;</code></td>
-                                <td><em>Este texto está en cursiva.</em></td>
-                            </tr>
-                            <tr>
-                                <td><del>Tachado</del></td>
-                                <td><code>&lt;del&gt; &lt;/del&gt;</code></td>
-                                <td><code>&lt;del&gt;Este texto está equivocado&lt;/del&gt;</code></td>
-                                <td><del>Este texto está equivocado</del></td>
-                            </tr>
-                            <tr>
-                                <td><strong>Cursiva + Negrita</strong></td>
-                                <td><code>&lt;strong&gt; &lt;/strong&gt; y &lt;em&gt; &lt;/em&gt;</code></td>
-                                <td><code>&lt;strong&gt;Este texto es &lt;em&gt;extremadamente&lt;/em&gt; importante&lt;/strong&gt;</code></td>
-                                <td><strong>Este texto es <em>extremadamente</em> importante</strong></td>
-                            </tr>
-                            <tr>
-                                <td><em>Negrita y Cursiva total</em></td>
-                                <td><code>&lt;strong&gt; &lt;/strong&gt; y &lt;em&gt; &lt;/em&gt;</code></td>
-                                <td><code>&lt;strong&gt;&lt;em&gt;Todo este texto es importante&lt;/em&gt;&lt;/strong&gt;</code></td>
-                                <td><strong><em>Todo este texto es importante</em></strong></td>
-                            </tr>
-                            <tr>
-                                <td>Subíndice</td>
-                                <td><code>&lt;sub&gt; &lt;/sub&gt;</code></td>
-                                <td><code>Este es un &lt;sub&gt;subíndice&lt;/sub&gt;</code></td>
-                                <td>Este es un <sub>subíndice</sub></td>
-                            </tr>
-                            <tr>
-                                <td>Superíndice</td>
-                                <td><code>&lt;sup&gt; &lt;/sup&gt;</code></td>
-                                <td><code>Este es un &lt;sup&gt;superíndice&lt;/sup&gt;</code></td>
-                                <td>Este es un <sup>superíndice</sup></td>
-                            </tr>
-                            <tr>
-                                <td>Subrayado</td>
-                                <td><code>&lt;ins&gt; &lt;/ins&gt;</code></td>
-                                <td><code>Este es un &lt;ins&gt;texto subrayado&lt;/ins&gt;</code></td>
-                                <td>Este es un <ins>texto subrayado</ins></td>
-                            </tr>
-                        </tbody>
-                    </table>
-                </p>
+                <table className="tabla__estilos__markdown">
+                    <thead>
+                        <tr>
+                            <th>Estilo</th>
+                            <th>Sintaxis</th>
+                            <th>Ejemplo</th>
+                            <th>Resultados</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td><strong>Negrita</strong></td>
+                            <td><code>&lt;strong&gt; &lt;/strong&gt;</code></td>
+                            <td><code>&lt;strong&gt;Este es un texto en negrita&lt;/strong&gt;</code></td>
+                            <td><strong>Este es un texto en negrita.</strong></td>
+                        </tr>
+                        <tr>
+                            <td><em>Cursiva</em></td>
+                            <td><code>&lt;em&gt; &lt;/em&gt;</code></td>
+                            <td><code>&lt;em&gt;Este texto está en cursiva&lt;/em&gt;</code></td>
+                            <td><em>Este texto está en cursiva.</em></td>
+                        </tr>
+                        <tr>
+                            <td><del>Tachado</del></td>
+                            <td><code>&lt;del&gt; &lt;/del&gt;</code></td>
+                            <td><code>&lt;del&gt;Este texto está equivocado&lt;/del&gt;</code></td>
+                            <td><del>Este texto está equivocado</del></td>
+                        </tr>
+                        <tr>
+                            <td><strong>Cursiva + Negrita</strong></td>
+                            <td><code>&lt;strong&gt; &lt;/strong&gt; y &lt;em&gt; &lt;/em&gt;</code></td>
+                            <td><code>&lt;strong&gt;Este texto es &lt;em&gt;extremadamente&lt;/em&gt; importante&lt;/strong&gt;</code></td>
+                            <td><strong>Este texto es <em>extremadamente</em> importante</strong></td>
+                        </tr>
+                        <tr>
+                            <td><em>Negrita y Cursiva total</em></td>
+                            <td><code>&lt;strong&gt; &lt;/strong&gt; y &lt;em&gt; &lt;/em&gt;</code></td>
+                            <td><code>&lt;strong&gt;&lt;em&gt;Todo este texto es importante&lt;/em&gt;&lt;/strong&gt;</code></td>
+                            <td><strong><em>Todo este texto es importante</em></strong></td>
+                        </tr>
+                        <tr>
+                            <td>Subíndice</td>
+                            <td><code>&lt;sub&gt; &lt;/sub&gt;</code></td>
+                            <td><code>Este es un &lt;sub&gt;subíndice&lt;/sub&gt;</code></td>
+                            <td>Este es un <sub>subíndice</sub></td>
+                        </tr>
+                        <tr>
+                            <td>Superíndice</td>
+                            <td><code>&lt;sup&gt; &lt;/sup&gt;</code></td>
+                            <td><code>Este es un &lt;sup&gt;superíndice&lt;/sup&gt;</code></td>
+                            <td>Este es un <sup>superíndice</sup></td>
+                        </tr>
+                        <tr>
+                            <td>Subrayado</td>
+                            <td><code>&lt;ins&gt; &lt;/ins&gt;</code></td>
+                            <td><code>Este es un &lt;ins&gt;texto subrayado&lt;/ins&gt;</code></td>
+                            <td>Este es un <ins>texto subrayado</ins></td>
+                        </tr>
+                    </tbody>
+                </table>
             </IntroductionSection>
             <IntroductionSection idSection="blockquote" hrefValue="#blockquote" titleSection={"Entrecomillado de texto"}>
                 <p>
                     Puede entrecomillar texto con <code><kbd>&gt;</kbd></code>.
                 </p>
-                <pre>
-                    <code>
-                        <p>Text that is not a quote</p>
-                        <blockquote><p>Text that is a quote</p></blockquote>
-                    </code>
-                </pre>
+                <p><code><kbd>&gt;</kbd> Texto a poner</code></p>
             </IntroductionSection>
             <IntroductionSection idSection="code" hrefValue="#code" titleSection="Código de cita">
                 <p>
                     Puedes procesar un código en línea o en bloque. Para el código en línea es usando las comillas invertidas
-                    <code>`</code>.
+                    `.
                 </p>
                 <p>
                     <code>Línea de código</code>
                 </p>
                 <p>
-                    Para los bloques de código es metiendo el contenido entre tres comillas invertidas <code>```</code>
+                    Para los bloques de código es metiendo el contenido entre tres comillas invertidas ```
                 </p>
-                <pre>
-                    <code>
-                        <p>Primero</p>
-                        <p>Segundo</p>
-                        <p>Tercero</p>
-                    </code>
-                </pre>
+                <p><code>```Contenido entre el bloque de código```</code></p>
             </IntroductionSection>
             <IntroductionSection idSection="enlaces" hrefValue="#enlaces" titleSection="Enlaces">
                 <p>
@@ -159,69 +140,45 @@ const IntroductionContent = () => {
                 <p>
                     Sintaxis básica para insertar imágeges:
                 </p>
-                <p>
-                    <code>
-                        ![Logo de Google](https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png)
-                    </code>
-                </p>
+                <p>![Logo de Google](https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png)</p>
                 <p>
                     Imagen con título (al pasar el ratón):
                 </p>
-                <p>
-                    <code>
-                        ![Logo de Google](https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png "Haz clic para ir a Google")
-                    </code>
-                </p>
+                <p>![Logo de Google](https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png "Haz clic para ir a Google")</p>
             </IntroductionSection>
             <IntroductionSection idSection="listas" hrefValue="#listas" titleSection="Listas">
                 <p>
                     Para listas desordenadas basta con colocar <code>-</code>,<code>*</code> o <code>+</code> antes de una o más líneas de texto:
                 </p>
-                <pre>
-                    <code>
-                        <p>- George</p>
-                        <p>* John Adams</p>
-                        <p>+ Thomas Jefferson</p>
-                    </code>
-                </pre>
+                <p><code>- George</code></p>
+                <p><code>* John Adams</code></p>
+                <p><code>+ Thomas Jefferson</code></p>
                 <p>
                     Para las listas ordenadas, antecede cada línea con un número:
                 </p>
-                <pre>
-                    <code>
-                        <p>1. George</p>
-                        <p>2. John Adams</p>
-                        <p>3. Thomas Jefferson</p>
-                    </code>
-                </pre>
+                <p><code>1. George</code></p>
+                <p><code>2. John Adams</code></p>
+                <p><code>3. Thomas Jefferson</code></p>
             </IntroductionSection>
             <IntroductionSection idSection="listasTareas" hrefValue="#listasTareas" titleSection="Listas de tareas">
                 <p>
                     Para crear una lista de tareas, debe añadir como prefijo un guion y espacio,
-                    seguido de <code>[ ]</code> a los elementos de la lista. Para marcar una tarea como completada,
-                    use <code>[x]</code>.
+                    seguido de [ ] a los elementos de la lista. Para marcar una tarea como completada,
+                    use [x].
                 </p>
-                <pre>
-                    <code>
-                        <p>- [x] #739</p>
-                        <p>- [ ] https://github.com/octo-org/octo-repo/issues/740</p>
-                        <p>- [ ] Add delight to the experience when all tasks are complete :tada:</p>
-                    </code>
-                </pre>
+                <p>- [x] #739</p>
+                <p>- [ ] https://github.com/octo-org/octo-repo/issues/740</p>
+                <p>- [ ] Add delight to the experience when all tasks are complete :tada:</p>
             </IntroductionSection>
             <IntroductionSection idSection="tablas" hrefValue="#tablas" titleSection="Tablas">
                 <p>
-                    En Markdown, las tablas se crean de una manera bastante simple usando pipes <code>(|)</code>
-                    para separar las columnas y guiones <code>(-)</code> para crear la fila de encabezado.
+                    En Markdown, las tablas se crean de una manera bastante simple usando pipes (|)
+                    para separar las columnas y guiones (-) para crear la fila de encabezado.
                 </p>
-                <pre>
-                    <code>
-                        <p>| Encabezado 1 | Encabezado 2 | Encabezado 3 |</p>
-                        <p>|--------------|--------------|--------------|</p>
-                        <p>| Fila 1, Col 1| Fila 1, Col 2| Fila 1, Col 3|</p>
-                        <p>| Fila 2, Col 1| Fila 2, Col 2| Fila 2, Col 3|</p>
-                    </code>
-                </pre>
+                <p>| Encabezado 1 | Encabezado 2 | Encabezado 3 |</p>
+                <p>|--------------|--------------|--------------|</p>
+                <p>| Fila 1, Col 1| Fila 1, Col 2| Fila 1, Col 3|</p>
+                <p>| Fila 2, Col 1| Fila 2, Col 2| Fila 2, Col 3|</p>
             </IntroductionSection>
         </div>
     );
